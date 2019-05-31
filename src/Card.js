e60306767e9d32fd75d8ce59443729dedea971d6
erampion <ch7128@gmail.com>
2019-05-30 19:57:17 -0700

    updating files

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
     return (
-        <div>
-            <p>This is the card component</p>
+      <div className='bg-light-silver dib br3 ba b--dark-gray bw1 pa2 ma1 grow shadow-5 tc'>
+        <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+        <div className='w5'>
+          <h3>{name}</h3>
+          <span>{email}</span>
         </div>
+      </div>
     )
 }
 
---
+++ src/SearchBox.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const SearchBox = () => {
+  return (
+    <div className='bb bw2 b--dark-gray pa3 mb2'>
+      <input 
+        className='pa3 ba b--dark-gray bg-lightest-blue'
+        type='search' 
+        placeholder='search robots'
+      />
+    </div>
+  );
+
+}
+
+
+export default SearchBox;
