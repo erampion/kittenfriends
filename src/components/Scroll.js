ffc1e375d5d38f0de24814a12807f3aa0624a942
erampion <ch7128@gmail.com>
2022-07-19 06:04:43 -0400

    updating the height of the scrollbar

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
   return (
-    <div style={{overflow: 'scroll', border: '1px solid black', height: '500px'}}>
+    <div style={{overflow: 'scroll', border: '1px solid black', height: '100rem'}}>
       {props.children}
     </div>
   );
