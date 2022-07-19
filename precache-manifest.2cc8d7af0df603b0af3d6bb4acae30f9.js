77db61c9771e13b5069a1976b281e89e91857ca4
erampion <ch7128@gmail.com>
2022-07-19 05:34:42 -0400

    Updates

---
+++ precache-manifest.2cc8d7af0df603b0af3d6bb4acae30f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c85ebb0a9716e996222",
+    "url": "/kittenfriends/static/css/main.3daeb840.chunk.css"
+  },
+  {
+    "revision": "2c85ebb0a9716e996222",
+    "url": "/kittenfriends/static/js/main.fcffc631.chunk.js"
+  },
+  {
+    "revision": "1634b392544e3f28d337",
+    "url": "/kittenfriends/static/js/runtime~main.7fd319c0.js"
+  },
+  {
+    "revision": "6523f22b1a5c12b7ac36",
+    "url": "/kittenfriends/static/css/2.659289d8.chunk.css"
+  },
+  {
+    "revision": "6523f22b1a5c12b7ac36",
+    "url": "/kittenfriends/static/js/2.f2c8162a.chunk.js"
+  },
+  {
+    "revision": "510535223dcd279f3967",
+    "url": "/kittenfriends/static/js/3.e9d442d7.chunk.js"
+  },
+  {
+    "revision": "11bee833674b0a327452a835d38cccca",
+    "url": "/kittenfriends/static/media/VacerSerifBoldPersonal.11bee833.woff"
+  },
+  {
+    "revision": "8a3b2559cc5d406169964a6f2a6399d1",
+    "url": "/kittenfriends/index.html"
+  }
+];
